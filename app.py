abaa96df10c23d1f891e61857123dec290e243c1
Adam Jimenez <jimenez.adam0@gmail.com>
2019-04-23 20:36:21 -0400

    port 80

--- app.py
+++ app.py
@@ -10,3 +10,6 @@ def kebac():
     if french_str is not None:
         return k.convert_input(french_str)
     return "salu toua! ta mal utilise lapi! fait ?french=kekchose"
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=80)
