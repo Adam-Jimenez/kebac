bdecfd0a79a79f9fb8da08677302155a4b5e91ad
Adam Jimenez <jimenez.adam0@gmail.com>
2019-04-23 20:33:04 -0400

    procfile

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 from kebac import Kebac
 k = Kebac()
 app = Flask(__name__)
@@ -6,4 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 def kebac():
     french_str = request.args.get('french')
-    return k.convert_input(french_str)
+    if french_str is not None:
+        return k.convert_input(french_str)
+    return "salu toua! ta mal utilise lapi! fait ?french=kekchose"
