32969f27e9119aef6756709a60b07814b9bc1883
Adam Jimenez <jimenez.adam0@gmail.com>
2019-04-23 20:25:50 -0400

    finish cleanup

--- find_invalid.py
+++ find_invalid.py
@@ -1,11 +1,9 @@
 from kebac import Kebac
 with open("dict.txt") as infile:
-    for line in infile:
+    for ln, line in enumerate(infile, 1):
         word, phonemes = line.split("===")
         phonemes = phonemes.replace("\n", "").split("_")
         for p in phonemes:
             if p not in Kebac.PHONEMES:
-                print(p)
-                print(word)
-                exit()
+                print(p, word, "line:",ln)
 
